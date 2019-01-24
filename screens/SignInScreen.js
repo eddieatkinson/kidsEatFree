c39cb75df7312e20394561b96a7dc2db1a4d2caf
Eddie Atkinson <eddiebatkinson@gmail.com>
2019-01-24 14:45:49 -0500

    set up initial route to sign in screen

---
+++ screens/SignInScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class SignInScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Sign in screen</Text>
+      </View>
+    )
+  }
+}
+
+export default SignInScreen;
