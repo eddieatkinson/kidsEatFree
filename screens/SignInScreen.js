763f91d0b7d4d28169a306fc9369acb63e07d56f
Eddie Atkinson <eddiebatkinson@gmail.com>
2019-01-26 15:10:26 -0500

    removed native-base, added elements, re-did login

--- screens/SignInScreen.js
+++ screens/SignInScreen.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { View, Text, TextInput, Form } from 'react-native';
+import { FormLabel, FormInput, FormValidationMessage, Button } from 'react-native-elements';
 
 class SignInScreen extends Component {
   render() {
     return (
       <View>
-        <TextInput
-          placeholder="Username"
-        />
-        <TextInput
-          placeholder="Password"
+        <FormLabel>Username</FormLabel>
+        <FormInput />
+        <FormLabel>Password</FormLabel>
+        <FormInput />
+        <Button
+          title='Submit'
+          backgroundColor='green'
+          borderRadius={5}
         />
       </View>
     )
